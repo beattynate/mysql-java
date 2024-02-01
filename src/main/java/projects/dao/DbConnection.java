6240d342131e69cbbe6f277b1f97e18c50b5b47e
beattynate <natebeatty@live.com>
2024-02-01 08:25:44 -0700

    week 9 commit

--- src/main/java/projects/dao/DbConnection.java
+++ src/main/java/projects/dao/DbConnection.java
@@ -1,23 +1,23 @@
-package recipes.dao;
+package projects.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import recipes.exception.DbException;
+import projects.exception.DbException;
 
 public class DbConnection {
-	private static final String SCHEMA	= "recipes";
-	private static final String USER = "recipes";
-	private static final String PASSWORD	= "recipes";
+	private static final String SCHEMA	= "projects";
+	private static final String USER = "projects";
+	private static final String PASSWORD	= "projects";
 	private static final String HOST	= "localhost";
 	private static final int PORT = 3306;
-	
+
 	public static Connection getConnection() {
-		String url = String.format("jdbc:mysql://%s:%d/%s?user=%s&password=%s&useSSL=false", 
+		String url = String.format("jdbc:mysql://%s:%d/%s?user=%s&password=%s&useSSL=false",
 				HOST, PORT, SCHEMA, USER, PASSWORD);
-		System.out.println("Connecting with url=" + url);
-		
+		//System.out.println("Connecting with url=" + url);
+
 		try {
 			Connection conn = DriverManager.getConnection(url);
 			System.out.println("Successfully obtained connection!");
--- src/main/java/projects/exception/DbException.java
+++ src/main/java/projects/exception/DbException.java
@@ -1,25 +1,25 @@
-package recipes.exception;
+package projects.exception;
 
 @SuppressWarnings("serial")
 public class DbException extends RuntimeException {
 
 	public DbException() {
-		
+
 	}
 
 	public DbException(String message) {
 		super(message);
-		
+
 	}
 
 	public DbException(Throwable cause) {
 		super(cause);
-		
+
 	}
 
 	public DbException(String message, Throwable cause) {
 		super(message, cause);
-		
+
 	}
 
 }
